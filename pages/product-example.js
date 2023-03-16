bdcd938ba6bbaa2eedfa6e6e0d67aae8fe11219d
Nichlolas Tillman <ntillman@protonmail.com>
2023-03-15 20:12:34 -0500

    testing snipcart

---
+++ pages/product-example.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+const ProductExample = () => {
+  
+  return (
+    <div>Product Name
+      <button
+      class="snipcart-add-item"
+      data-item-id="8eac9f4a-c3f9-4382-9b34-de9a1efde401"
+      data-item-name="IM product- Test"
+      data-item-version="v1"
+      data-item-price="25.99"
+      data-item-image="https://asimstudioart.philosofiq.com/wp-content/uploads/2023/02/prod_image-test-450x450.jpg"
+    >
+      Add to Cart
+    </button>
+    </div>
+  )
+}
+
+export default ProductExample;
