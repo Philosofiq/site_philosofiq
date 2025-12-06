ce5e91218baad0a66ceb15950494f67988fbbf9a
Nichlolas Tillman <ntillman@protonmail.com>
2025-12-06 10:21:14 -0600

    update patch for cve advisory from next

--- components/Banner.js
+++ components/Banner.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Headline from "../components/headline";
+import Headline from "./Headline";
 import BannerIntro from "./BannerIntro";
 import CTA from "./CTA";
 
